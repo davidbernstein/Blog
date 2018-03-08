4e5683102d01d875c7e2c6f039095b207c2b000f
David Bernstein <david.h.bernstein@gmail.com>
2018-03-08 12:21:02 -0800

    Experimenting with dir structure and build
    Builds but I’m not sure it’s really right

---
+++ cpp/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+#include "src/2018/march_10/march_10_2018.h"
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
---
+++ cpp/src/2018/march_10/march_10_2018.cpp
@@ -0,0 +1,6 @@
+#include "march_10_2018.h"
+
+bool test_function(void)
+{
+	return true;
+}
